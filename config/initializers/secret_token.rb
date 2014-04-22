0c9f269a2ce707b13e07fe568f79ad96736cf294
Alex Miller <alex.burr.miller@gmail.com>
2014-04-22 12:16:19 -0400

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeshipTest::Application.config.secret_token = '40dceabe840e7f733705845acb460eee852c04be2c909f0226167ca4395afd9d13170636cd3fd2820cf2184a48cf45101d042bad68a674fc668737c801982026'
